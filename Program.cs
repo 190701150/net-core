f83b70f4df16cf88d19cd121163553e9194373df
190701150 <190701150@stu.istinye.edu.tr>
2022-05-10 20:10:08 +0300

    deneme

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace deneme123
+{
+    class Program
+    {
+        static void Main(string[] args)
+      {
+            Console.WriteLine("Hello trstrfsWorld!");
+
+
+        }
+    }
+}
